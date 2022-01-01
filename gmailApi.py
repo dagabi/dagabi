407e1e1114006b8c607a2cdaf3be43c066b848da
GABRIELPC\Gabriel <dagabi@gmail.com>
2022-01-01 21:17:50 +0200

    google api read messages

--- gmailApi.py
+++ gmailApi.py
@@ -15,17 +15,25 @@ def main():
     service = build('gmail', 'v1', http=creds.authorize(Http()))
     
     # Call the Gmail API to fetch INBOX
-    results = service.users().messages().list(userId='me',labelIds = ['INBOX'], q="FROM:subscriptions@lists.juno.co.uk").execute()
+    results = service.users().messages().list(userId='me',labelIds = ['INBOX'], q="FROM:subscriptions@lists.juno.co.uk after:2021/12/25").execute()
     messages = results.get('messages', [])
     
+    fileIdx = 0
 
     if not messages:
-        print( "No messages found.")
+        print( "No messages found .")
     else:
         print("Message snippets:")
         for message in messages:
-            msg = service.users().messages().get(userId='me', id=message['id']).execute()
-            print(base64.b64decode(msg['payload']['body']['data'].encode('utf8')).decode('utf8'))
+            msg = service.users().messages().get(userId='me', id=message['id'], format='full').execute()
+            content = base64.urlsafe_b64decode(msg['payload']['body']['data']).decode('utf-8')
+            subject = next(obj for obj in msg['payload']['headers'] if obj['name'] == 'Subject')
+            messageSubject = ''
+            if type(subject) == dict:
+                messageSubject = subject['value']
+            with open(f"message{fileIdx}.htm", 'w') as f:
+                f.write(content)
+            fileIdx = fileIdx + 1
 
 if __name__ == '__main__':
     main()
